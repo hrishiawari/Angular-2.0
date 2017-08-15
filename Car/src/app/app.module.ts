4e3b647a39a2ecb6a42651434dcd109e117f56af
HrishiAwari <hrishiawari007@gmail.com>
2017-08-15 13:10:07 +0530

    Importing Table

--- Car/src/app/app.module.ts
+++ Car/src/app/app.module.ts
@@ -8,20 +8,34 @@ import { DirectivesComponent } from './directives/directives.component';
 
 import { SizerDirective } from './directives/sizer.directive';
 import { EffectDirective } from './directives/effect.directive';
+import { TableComponent } from './table/table.component';
+// import { Ng2SmartTableModule } from 'ng2-smart-table';
 
+ // import { LocalRefsComponent } from './local-refs/local-refs.component';
+ // import { DrectivesComponent } from './drectives/drectives.component';
+ // import { MyAttrDirDirective } from './drectives/my-attr-dir.directive';
+// import { NavBarDirective } from './drectives/nav-bar.directive';
+ //import { NavBarCompComponent } from './drectives/nav-bar-comp.component';
+ import { Ng2TableModule } from 'ng2-table/ng2-table';
+ //import { ExtraTabComponent } from './drectives/extra-tab.component';
+ //import {RatingModule} from 'ng2-rating'
 @NgModule({
   declarations: [
     AppComponent,
     EngineComponent,
     CarComponent,
     DirectivesComponent,
-    
+  
     SizerDirective,
     
-    EffectDirective
+    EffectDirective,
+    
+    TableComponent,
+      // Ng2SmartTableModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+     Ng2TableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
